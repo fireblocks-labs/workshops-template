8717e2c0613166f5f6017da05502a8b6aa7fd7e7
SlavaSereb <slava@fireblocks.com>
2024-08-28 17:54:08 +0300

    change layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Layout from "./components/Layout";
 import { MainPage } from "./components/MainPage";
 
 export default function Home() {
   
   return (
-    <Layout>
-      <MainPage/>
-    </Layout>
+    <MainPage/>
   );
 }
